c1f394f9b58f99e67ed3718069d4aaa101945fdf
Sethembiso <maissel49@gmail.com>
2025-11-03 01:19:36 +0200

    Initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,3 +2,9 @@
 import axios from 'axios';
 const api = axios.create({ baseURL: '/' }); // proxied to backend in vite config
 export default api;
+const API = import.meta.env.VITE_API_URL;
+export const getStudents = async () => {
+  const res = await fetch(`${API}/students`);
+  return res.json();
+};
+
